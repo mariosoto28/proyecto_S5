31ae73cd13c978649746628b557bda9b20cc175c
mariosoto28 <miguelsotozea950@gmail.com>
2024-05-27 11:07:57 -0500

    Agregando

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/SessionManager.java
@@ -0,0 +1,23 @@
+
+import lombok.Data;
+
+
+
+
+
+
+    @Data
+    
+    public class SessionManager {
+
+        static SessionManager instance;
+        Long userId;
+
+        public static synchronized SessionManager getInstance() {
+            if (instance == null) {
+                instance = new SessionManager();
+            }
+            return instance;
+        }
+    }
+
---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/servicio/ProductoService.java
@@ -0,0 +1,65 @@
+package pe.edu.upeu.syscenterlife.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.com.syscenterlife.autocomp.ModeloDataAutocomplet;
+import pe.edu.upeu.syscenterlife.modelo.Producto;
+import pe.edu.upeu.syscenterlife.repositorio.ProductoRepository;
+import pe.edu.upeu.syscenterlife.util.ErrorLogger;
+
+@Service
+public class ProductoService {
+    
+    ErrorLogger log=new ErrorLogger("ClienteService.class");
+
+    @Autowired
+    ProductoRepository repository;
+
+    // Crear
+    public Producto guardarEntidad(Producto producto) {
+        return repository.save(producto);
+    }
+
+    // Leer todos los elementos
+    public List<Producto> listarEntidad() {
+        return repository.findAll();
+    }
+
+    // Actualizar
+    public Producto actualizarEntidad(Producto producto) {
+        return repository.save(producto);
+    }
+
+    // Eliminar
+    public void eliminarEntidad(Long id) {
+        repository.deleteById(id);
+    }
+
+    // Buscar por ID
+    public Producto buscarEntidad(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<ModeloDataAutocomplet> listAutoCompletProducto(String nombre) {
+        List<ModeloDataAutocomplet> listarProducto = new ArrayList<>();
+
+        try {
+            for (Producto producto : repository.listAutoCompletProducto(nombre + "%")) {
+                ModeloDataAutocomplet data = new ModeloDataAutocomplet();
+                ModeloDataAutocomplet.TIPE_DISPLAY = "ID";
+                data.setIdx(producto.getNombre());
+                data.setNombreDysplay(String.valueOf(producto.getIdProducto()));
+                data.setOtherData(producto.getPu() + ":" + producto.getStock());
+                listarProducto.add(data);
+            }
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "create", e);
+        }
+
+        return listarProducto;
+    }
+
+}
